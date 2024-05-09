9eef4e5baf4345a9da9b72028d852dc3dff0e0f7
SPIDER <162958422+spidervsyou@users.noreply.github.com>
2024-05-09 22:16:28 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import Directors from './pages/Directors';
+import Actors from './pages/Actors';
 
-
-const routes = [
-   
-  ];
+const routes = (
+  <>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/movie/:id" component={Movie} />
+    <Route exact path="/directors" component={Directors} />
+    <Route exact path="/actors" component={Actors} />
+  </>
+);
 
 export default routes;
