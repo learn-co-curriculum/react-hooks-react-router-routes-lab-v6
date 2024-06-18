855ec6cfd85e093ac158d40469acebf87a086f94
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-06-18 11:22:22 -0400

    Complete all tests

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,39 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import NavBar from "../components/NavBar"
 
 function Movie() {
+  const [movie, setMovie] = useState({})
+  const params = useParams()
+  const movieId = params.id
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+      .then((r) => r.json())
+      .then((data) => setMovie(data))
+      .catch((error) => console.error(error))
+  }, [movieId])
+
+  if (!movie.title) {
+    return <h1>Loading...</h1>
+  }
+
+  const displayGenreList = movie.genres
+    ? movie.genres.map((genre, index) => <span key={index}>{genre}</span>)
+    : null
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        {displayGenreList}
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Movie;
+export default Movie
