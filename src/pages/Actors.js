139885700cc5cead141ac29de1f575cbecbae6ab
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-18 13:33:47 -0400

    Add routes for all pages

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,16 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
+import NavBar from "../components/NavBar"
 
 function Actors() {
+  const [actors, setActors] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/actors")
+      .then((r) => r.json())
+      .then((data) => setActors(data))
+      .catch((error) => console.error(error))
+  }, [])
+
+  const actorList = actors.map((actor) => {
+    return (
+      <article key={actor.id}>
+        <h1>{actor.name}</h1>
+        <ul>
+          {actor.movies.map((movie) => (
+            <li key={movie}>{movie}</li>
+          ))}
+        </ul>
+      </article>
+    )
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        <div>{actorList}</div>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Actors;
+export default Actors
