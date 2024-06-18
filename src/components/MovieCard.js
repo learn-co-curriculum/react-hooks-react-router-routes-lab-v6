1cabaf45e48fbe5c1fe5f162e30849aafa4c9098
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-18 13:56:00 -0400

    Display movie titles on home page

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,15 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom"
 
-function MovieCard({title}) {
-  
+function MovieCard({ movie }) {
+  console.log("movieCard", movie)
+  const { id, title } = movie
+
   return (
     <article>
-        <h2>{title}</h2>
-        {/* What should go here? */}
+      <h2>{title}</h2>
+      {/* What should go here? */}
     </article>
-  );
-};
+  )
+}
 
-export default MovieCard;
+export default MovieCard
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
+import NavBar from "../components/NavBar"
+import MovieCard from "../components/MovieCard"
 
 function Home() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((response) => response.json())
+      .then((movieData) => setMovies(movieData))
+  }, [])
+
+  const movieList = movies.map((movie) => (
+    <MovieCard key={movie.id} movie={movie} />
+  ))
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        {movieList}
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
