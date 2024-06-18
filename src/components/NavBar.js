022cddd1dd986ddfd688186f00c985880602d3d2
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-18 12:00:28 -0400

    Display Home Page with NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,20 @@
-import { NavLink } from "react-router-dom";
-import "./NavBar.css";
+import { NavLink } from "react-router-dom"
+import "./NavBar.css"
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
+    <nav>
+      <NavLink to="/" className="navbar">
+        Home
+      </NavLink>
+      <NavLink to="/directors" className="navbar">
+        Directors
+      </NavLink>
+      <NavLink to="/actors" className="navbar">
+        Actors
+      </NavLink>
     </nav>
-    );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
