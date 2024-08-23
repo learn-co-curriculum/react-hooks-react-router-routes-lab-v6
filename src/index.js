e61477aa7d10d13b5d4da9c1c7549e370d504f4f
Shukri Abdi <shukri751@gmail.com>
2024-08-24 02:28:57 +0300

    react-hooks-state-events-mini-project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import "./index.css";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
