c1dbeb14217b9865c01c08b0d5fed247cba36766
Emily Kanarek <emilyakanarek@gmail.com>
2025-06-17 11:47:32 -0400

    Show movie list on home page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,12 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function MovieCard({title}) {
-  
+function MovieCard({ title, id }) {
   return (
     <article>
-        <h2>{title}</h2>
-        {/* What should go here? */}
+      <h2>{title}</h2>
+      <Link to={`/movies/${id}`}>Go to movie page</Link>
     </article>
   );
-};
+}
 
 export default MovieCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((r) => r.json())
+      .then((movies) => setMovieList(movies));
+  }, []);
+
   return (
     <>
       <header>
         <NavBar />
       </header>
-      <main>{/* Info goes here! */}</main>
+      <main>
+        <h1>Home Page</h1>
+        {movieList.map((movie) => (
+          <MovieCard key={movie.id} title={movie.title} id={movie.id} />
+        ))}
+      </main>
     </>
   );
 }
