e14b086126daf5c4d242f56b03145c7b48f4452d
Emily Kanarek <emilyakanarek@gmail.com>
2025-06-17 11:34:16 -0400

    Set up Nav bar

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Movie() {
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
+      <main>{/* Movie info here! */}</main>
     </>
   );
-};
+}
 
 export default Movie;
