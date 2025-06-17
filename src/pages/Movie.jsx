7934c5f88106b07a1f7d376009b889a1bd29262c
Emily Kanarek <emilyakanarek@gmail.com>
2025-06-17 11:55:28 -0400

    Render movie data on movie page

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,13 +1,34 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const [genres, setGenres] = useState([]);
+  const params = useParams();
+  const movieId = params.id;
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+      .then((r) => r.json())
+      .then((data) => {
+        setMovie(data);
+        setGenres(data.genres);
+      });
+  }, [movieId]);
+
   return (
     <>
       <header>
         <NavBar />
       </header>
-      <main>{/* Movie info here! */}</main>
+      <main>
+        <h1>{movie.title}</h1>
+        <p>{movie.time} minutes</p>
+        {genres.map((genre) => (
+          <span key={genre}>{genre}</span>
+        ))}
+      </main>
     </>
   );
 }
