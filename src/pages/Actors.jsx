1ca1a39842e407aa2641d7ebdef8b68c130da6fc
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-30 00:07:53 -0400

    Render each actors name to page and display their list of movies

--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -2,12 +2,33 @@ import {useEffect, useState} from "react"
 import NavBar from "../components/NavBar"
 
 function Actors() {
+  const [actors, setActors] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/actors")
+      .then((r) => r.json())
+      .then((data) => setActors(data))
+      .catch((error) => console.error(error))
+  }, [])
+
   return (
     <>
       <header>
         <NavBar />
       </header>
-      <main>{/* Actor info here! */}</main>
+      <main>
+        <h1>Actors Page</h1>
+        {actors.map((actor) => (
+          <article key={actor.id}>
+            <h2>{actor.name}</h2>
+            <ul>
+              {actor.movies.map((movie) => (
+                <li key={movie}>{movie}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
+      </main>
     </>
   )
 }
