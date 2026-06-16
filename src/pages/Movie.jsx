93286127f958d954db73773503946f2952583cc2
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-06-16 11:27:44 -0400

    Routes lab complete

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,34 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
-  return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
-    </>
-  );
-};
+	const { id } = useParams();
+	const [movie, setMovie] = useState(null);
+
+	useEffect(() => {
+		fetch(`http://localhost:4000/movies/${id}`)
+			.then((r) => r.json())
+			.then((data) => setMovie(data))
+			.catch((error) => console.log(error));
+	}, [id]);
+
+	if (!movie) return null;
+
+	return (
+		<>
+			<header>
+				<NavBar />
+			</header>
+			<main>
+				<h1>{movie.title}</h1>
+				<p>{movie.time}</p>
+				{movie.genres.map((genre) => (
+					<span key={genre}>{genre}</span>
+				))}
+			</main>
+		</>
+	);
+}
 
 export default Movie;
