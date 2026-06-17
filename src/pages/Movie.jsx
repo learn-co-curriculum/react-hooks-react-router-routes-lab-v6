3f6212142ba3e7a033f787f2b48c92b5f9608112
Thomas Correia <thomas.correiap@gmail.com>
2026-06-17 11:29:41 -0400

    Completed React Routes Lab

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,35 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie, setMovie] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then((res) => res.json())
+      .then((data) => setMovie(data));
+  }, [id]);
+
+  if (!movie) return null;
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
+
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+
+        {movie.genres.map((genre) => (
+          <span key={genre}>{genre}</span>
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Movie;
